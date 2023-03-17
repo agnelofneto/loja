25413ee999b204263ec4e6422b3a08bfa7e5dbd5
agnelo <agnelofneto@gmail.com>
2023-03-17 09:32:33 +0000

    navbar com problema de renderização

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,12 +1,25 @@
 
+/* 
 import Login from '@/components/login';
 
 const Home = () => {
   return (
    <Login />
+  );
+};
 
-   
+export default Home;
+*/
 
+
+import Navbar from '@/components/navbar';
+
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+      
+    </>
   );
 };
 

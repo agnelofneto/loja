3ae14773cdb69324540c05775e9e8a990b612e80
agnelo <agnelofneto@gmail.com>
2023-03-27 20:42:13 +0100

    update

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -1,21 +1,26 @@
-import { ReactNode } from "react";
+import NextLink from "next/link";
 import {
   Box,
   Flex,
   HStack,
-  Link,
+  Link as ChakraLink,
   IconButton,
   useDisclosure,
   useColorModeValue,
   Stack,
+  Button,
 } from "@chakra-ui/react";
 
 import { HamburgerIcon, CloseIcon } from "@chakra-ui/icons";
 
+import Image from "next/image";
+
+import logo from "../assets/agnelo.png";
+
 const Links = ["Dashboard", "Projects", "Team"];
 
 const LinkWrapper = ({ children }) => (
-  <Link
+  <ChakraLink
     px={2}
     py={1}
     rounded={"md"}
@@ -26,7 +31,7 @@ const LinkWrapper = ({ children }) => (
     href={"#"}
   >
     {children}
-  </Link>
+  </ChakraLink>
 );
 
 export function Navbar() {
@@ -34,31 +39,61 @@ export function Navbar() {
 
   return (
     <>
-      <Box bg={useColorModeValue("gray.100", "gray.900")} px={4}>
+      {/* Desktop */}
+      <Box
+        bg={useColorModeValue("gray.100", "gray.900")}
+        px={4}
+        display={{ base: "none", md: "block" }}
+      >
+        <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
+          <HStack spacing={8} alignItems={"center"}>
+            <Image src={logo} width={30} height={30} align="left" />
+            <LinkWrapper>{Links[0]}</LinkWrapper>
+            <LinkWrapper>{Links[1]}</LinkWrapper>
+            <LinkWrapper>{Links[2]}</LinkWrapper>
+          </HStack>
+
+          <NextLink href="/login">
+            <Box>
+              <Button bg="blue.400" color="white">
+                Login
+              </Button>
+            </Box>
+          </NextLink>
+        </Flex>
+      </Box>
+
+      {/* Mobile */}
+      <Box
+        bg={useColorModeValue("gray.100", "gray.900")}
+        px={4}
+        display={{ base: "block", md: "none" }}
+      >
         <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
           <IconButton
             size={"md"}
             icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
             aria-label={"Open Menu"}
-            display={{ md: "none" }}
             onClick={isOpen ? onClose : onOpen}
           />
-          <HStack spacing={8} alignItems={"center"}>
-            <Box>Logo</Box>
-            <HStack spacing={4} display={{ base: "none", md: "flex" }}>
-              {Links.map((link) => (
-                <LinkWrapper key={link}>{link}</LinkWrapper>
-              ))}
-            </HStack>
-          </HStack>
+          <Box mx="auto">
+            <Image src={logo} width={30} height={30} />
+          </Box>
+          <Box>
+            <NextLink href="/login">
+              <Button bg="blue.400" color="white">
+                Login
+              </Button>
+            </NextLink>
+          </Box>
         </Flex>
 
         {isOpen ? (
-          <Box pb={4} display={{ md: "none" }}>
+          <Box pb={4}>
             <Stack spacing={4}>
-              {Links.map((link) => (
-                <LinkWrapper key={link}>{link}</LinkWrapper>
-              ))}
+              <LinkWrapper>{Links[0]}</LinkWrapper>
+              <LinkWrapper>{Links[1]}</LinkWrapper>
+              <LinkWrapper>{Links[2]}</LinkWrapper>
             </Stack>
           </Box>
         ) : null}
